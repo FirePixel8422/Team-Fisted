271005ff09746e1bb7f2617fa9062982bc3a2219
Atmojomojo <rotastisch@gmail.com>
2024-10-15 17:02:00 +0200

    Scale chase enemy on start

---
+++ Assets/Art/Character/ChaseStart.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseStart : MonoBehaviour
+{
+    public float scale, growSpeed;
+    public GameObject arms;
+    public void OnEnable()
+    {
+        scale = 0;
+        arms.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(Vector3.zero, new Vector3(1, 1, 1), scale);
+        if (scale < 1)
+        {
+            scale += growSpeed;
+        }
+        else
+        {
+            arms.SetActive(true);
+        }
+        //gameObject.GetComponent<ParticleSystem>().startColor.
+    }
+}
