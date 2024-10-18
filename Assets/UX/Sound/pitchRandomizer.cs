9096db8f1ee6ab75e89a78e32d696da926f6cb04
JelmerHofstee2 <133031977+JelmerHofstee2@users.noreply.github.com>
2024-10-18 15:21:31 +0200

    sounds added

--- Assets/UX/Sound/pitchRandomizer.cs
+++ Assets/UX/Sound/pitchRandomizer.cs
@@ -6,34 +6,52 @@ using UnityEngine;
 public class pitchRandomizer : MonoBehaviour
 {
     public int arrayRange;
+
+
     public AudioClip[] audioClip; 
+
     public AudioClip currentAudioClip;
+
     public int randomNumber;
+
     public AudioSource source;
+
     public int listInt = 1;
+
     public float timer;
     public float soundCooldown;
+
     public int minCooldown;
     public int maxCooldown;
+
+    public bool chaseMode;
+    public int chaseMinCooldown;
+    public int chaseMaxCooldown;
+
 
     // Start is called before the first frame update
     void Start()
     { 
         
     }
-
-        
-    
-
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+        //if (chaseMode == true)
+        //{
+        //    minCooldown += chaseMinCooldown;
+        //    maxCooldown += chaseMaxCooldown;
+        //}
+        //else
+        //{
+        //    minCooldown -= chaseMinCooldown;
+        //    maxCooldown -= chaseMaxCooldown;
+        //}
         if (timer > soundCooldown)
         {
             timer = 0;
                         
-            
             randomNumber = Random.Range(0, arrayRange);
             currentAudioClip = audioClip[randomNumber];
             PlayTrack(currentAudioClip);
